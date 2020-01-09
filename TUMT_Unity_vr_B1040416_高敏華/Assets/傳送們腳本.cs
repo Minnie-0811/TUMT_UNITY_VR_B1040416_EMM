a57214baddbe2cc96439db22184ef44d29dfe376
kao84293@gmail.com <kao84293@gmail.com>
2020-01-09 11:28:43 +0800

    01

---
+++ TUMT_Unity_vr_B1040416_高敏華/Assets/傳送們腳本.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class 傳送們腳本 : MonoBehaviour 
+{
+    public Animator animatorConrol;
+}
---
+++ TUMT_Unity_vr_B1040416_高敏華/Assets/腳本.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class 腳本 : MonoBehaviour
+{
+    //公開必且參數零或是一個 
+    public void ThrowPickUp()
+    {
+        print("撿起物件");
+    }
+
+    public void ThrowDetach()
+    {
+        print("放開物件");
+    }
+
+    public void ThrowHeld()
+    {
+        print("拿起物件");
+    }
+}
